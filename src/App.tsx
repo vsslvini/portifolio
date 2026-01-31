cd1da4f31a7a58691b862bef911606e40491ca4b
Vinicius Alencar <vsslviniciussousa@gmail.com>
2026-01-31 00:17:49 -0400

    feat: importa sessão de project no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Home from "./pages/Home/Home";
 import About from "./pages/Home/sections/About/About";
+import Projects from "./pages/Home/sections/Projects/Projects";
 import TechStack from "./pages/Home/sections/TechStack/TechStack";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <Home />
       <About />
       <TechStack />
+      <Projects/>
     </>
   );
 };
